56290bb09b452eeeb616b7d48e92e5267c8653c5
Your Name <you@example.com>
2021-05-31 17:17:26 +0545

    updated one fime

---
+++ routes/contact.js
@@ -0,0 +1,21 @@
+//var express = require('express');
+//var router = express.Router();
+//
+///* GET home page. */
+//router.get('/', function(req, res, next) {
+//  res.render('index', { title: 'Express' });
+//});
+//
+//module.exports = router;
+
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req,res, next){
+    res.render('contact');
+
+
+});
+
+module.exports = router;
