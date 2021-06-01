dde26b8be0189b719d1f6422e22a65192f38cc09
Your Name <you@example.com>
2021-06-01 08:35:34 +0545

    new file

--- routes/contact.js
+++ routes/contact.js
@@ -13,7 +13,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res, next){
-    res.render('contact');
+    res.render('contact', { title: 'Nepal Networks - Contact' });
 
 
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nepal Networks' });
+  res.render('index', { title: 'Nepal Networks - Home' });
 });
 
 module.exports = router;
